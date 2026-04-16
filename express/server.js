795ea92d3e1ac5b37875294930e7ea39cd5c8da4
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-04-16 09:53:50 +0300

    Add project on Express

---
+++ express/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "/index.html"));
+})
+
+app.listen(port, () => {
+    console.log(`Server running 'http://localhost:3000/'`)
+});
