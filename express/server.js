f809beb1e9f4d08fdcb6441ec9c33e5ce27e9b7f
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-04-22 11:03:14 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { initDatabase } from './database/database.js';
+
+import apiRoutes from "./routes/api.js";
+import pageRoutes from "./routes/pages.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+await initDatabase();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "/index.html"));
-})
+app.use("/", pageRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
     console.log(`Server running 'http://localhost:3000/'`)
